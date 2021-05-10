f8f1258a720752002d4886558d280fa4327efa11
isagomachado <82418322+isagomachado@users.noreply.github.com>
2021-05-10 19:46:28 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var valorEmGrausC = prompt("insira a temperatura em graus Celcius")
+
+var valorEmGrausCNumero = parseFloat(valorEmGrausC)
+
+var valorEmGrausF = (valorEmGrausCNumero * 1.08) + 32 
+
+
+alert(valorEmGrausF)
